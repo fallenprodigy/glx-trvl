f99220447052c56e468196ea2877cfb0b750f207
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-08-08 23:29:26 +0400

    footer section

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../componenets/Footer";
 import Navbar from "../componenets/Navbar";
 import Video from "../componenets/Video";
 
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Navbar />
       <Video />
+      <Footer />
     </div>
   );
 };
