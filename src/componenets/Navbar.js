7e4dfed67be30b987a5c9864d9aa84e4fa978387
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-08-08 21:15:50 +0400

    mobile nav bar done

---
+++ src/componenets/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <h1>GLX TRVL</h1>
+      </Link>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/pricing">Pricing</Link>
+        </li>
+        <li>
+          <Link to="/training">Training</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="hambruger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
