018cd9a4bd1613e30e37f4678fad12fef57f04f0
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-08-08 23:35:50 +0400

    pricing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./routes/Home";
 import { Route, Routes } from "react-router-dom";
+import Pricing from "./routes/Pricing";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
       </Routes>
     </>
   );
---
+++ src/routes/Pricing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Navbar from "../componenets/Navbar";
+import Footer from "../componenets/Footer";
+
+const Pricing = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Pricing;
