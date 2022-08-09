600ad72f0d31188cd57ed11bd826232d005e4765
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-08-09 20:14:41 +0400

    pricing page done

---
+++ src/componenets/HeroImage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HeroImage.css";
+
+const HeroImage = () => {
+  return (
+    <div className="hero-img">
+      <div className="heading">
+        <h1>PRICING</h1>
+        <p>Choose your trip.</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImage;
--- src/routes/Pricing.jsx
+++ src/routes/Pricing.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 import Navbar from "../componenets/Navbar";
+import HeroImage from "../componenets/HeroImage";
+import PricingCards from "../componenets/Pricing";
 import Footer from "../componenets/Footer";
 
 const Pricing = () => {
   return (
     <div>
       <Navbar />
+      <HeroImage />
+      <PricingCards />
       <Footer />
     </div>
   );
